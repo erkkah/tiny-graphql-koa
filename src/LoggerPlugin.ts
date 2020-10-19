b41ba9ef7492224d436bc5c273028a210261c074
Erik Agsjö <erik.agsjo@gmail.com>
2020-10-19 22:36:22 +0200

    More meat to logging

--- src/LoggerPlugin.ts
+++ src/LoggerPlugin.ts
@@ -1,12 +1,32 @@
-import { DocumentNode, ExecutionArgs } from "graphql";
+import { DocumentNode, ExecutionArgs, OperationDefinitionNode } from "graphql";
 import { SchemaTransform } from "@graphql-tools/utils";
 
 import { GraphQLPlugin, Executable } from ".";
 
 type LogFunction = (...msg: string[]) => void;
+interface LeveledLogger {
+    error?: LogFunction;
+    info?: LogFunction;
+    debug?: LogFunction;
+}
 
+interface LoggerPluginOptions {
+    logger?: LogFunction;
+    leveled?: LeveledLogger;
+    logQueries?: boolean;
+    debugLogFullQueries?: boolean;
+}
+
+/**
+ * Logs errors that occur during query execution, including those that
+ * are filtered by the `graphql-api-koa` errorHandler.
+ */
 export class LoggerPlugin implements GraphQLPlugin {
-    constructor(private readonly logger: LogFunction = console.log) { }
+    constructor(private readonly options: LoggerPluginOptions = { logger: console.log }) {
+        if (!options.leveled && !options.logger) {
+            options.logger = console.log;
+        }
+    }
 
     directives(): (string | DocumentNode)[] {
         return [];
@@ -19,17 +39,52 @@ export class LoggerPlugin implements GraphQLPlugin {
     wrapper = (next: Executable): Executable =>
         async (args: ExecutionArgs) => {
             try {
+                let requestStart = BigInt(0);
+
+                if (this.options.debugLogFullQueries) {
+                    this.debug(args.document.loc?.source?.body || "");
+                }
+
+                if (this.options.logQueries) {
+                    requestStart = process.hrtime.bigint();
+                }
+
                 const result = await next(args);
                 if (result.errors) {
                     const messages = result.errors.map((error) => `Error at "${error.path?.join(".")}": ${error.stack || error.message}`);
-                    this.logger(...messages);
+                    this.error(...messages);
+                }
+
+                if (this.options.logQueries) {
+                    const duration = Number(process.hrtime.bigint() - requestStart) / 1E6;
+                    const operations = args.document.definitions
+                        .filter((def): def is OperationDefinitionNode => def.kind === "OperationDefinition")
+                        .map((opDef: OperationDefinitionNode) => opDef.operation)
+                        .join(", ");
+                    this.info(`Executed ${operations} with ${result.errors ? "errors" : "success"} in ${duration.toPrecision(4)}ms`);
                 }
                 return result;
             } catch (error) {
-                this.logger(error);
+                this.error(error);
                 throw error;
             }
         };
+
+    private error(...messages: string[]): void {
+        const logger = this.options.leveled?.error || this.options.logger;
+        logger?.(...messages);
+    }
+
+    private info(...messages: string[]): void {
+        const logger = this.options.leveled?.info || this.options.logger;
+        logger?.(...messages);
+    }
+
+    private debug(...messages: string[]): void {
+        const logger = this.options.leveled?.debug || this.options.logger;
+        logger?.(...messages);
+    }
+
 }
 
 export default LoggerPlugin;
